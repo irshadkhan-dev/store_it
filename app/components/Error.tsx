3ba7ac6bfd008b518f9b8f144beebe76bddd3d38
Irshad khan <irshadkhan.devop@gmail.com>
2025-02-26 23:04:10 +0545

    added Error component

---
+++ app/components/Error.tsx
@@ -0,0 +1,11 @@
+import { CloudAlert } from "lucide-react";
+
+const ErrorComp = () => {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <CloudAlert className="animate-bounce h-12 w-12" />
+    </div>
+  );
+};
+
+export default ErrorComp;
